36074071e57949f466a0e438eb265fa9fd76bbcf
galind0-000 <144351770+galind0-000@users.noreply.github.com>
2023-09-07 08:05:42 -0600

    Add files via upload

---
+++ TS5-rgc_1010023.py
@@ -0,0 +1,15 @@
+print("trabajo supervisado-18")
+print("7/09/2023")
+print("Rodrigo Galindo Callen")
+
+sNombre= input("ingrese su nombre")
+sEdad= input("por favor ingreso su edad")
+sCarrera= input("por favor ingrese su carrera")
+sCarne= input("por favor ingresar su carne")
+
+print("nombre",sNombre)
+print("edad", sEdad)
+print("Carrera", sCarrera)
+print("carne", sCarne)
+
+print("soy" +sNombre, "tengo" +sEdad, "años y estudio la carrera de" +sCarrera, "Mi numero de carnet es: "+sCarne )
